7fdd9440a0b6029e525771fc187045a819cbe7b4
Nipo Sofela <niporainy@gmail.com>
2025-10-23 07:02:17 +0100

    Use environment variables for Firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -9,7 +9,7 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: 'AIzaSyC15Bpx4eDkioF_qRD20FLxVUxTCtcsELY',
+	apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
 	authDomain: 'healthinsurancedemo-d130b.firebaseapp.com',
 	projectId: 'healthinsurancedemo-d130b',
 	storageBucket: 'healthinsurancedemo-d130b.firebasestorage.app',
